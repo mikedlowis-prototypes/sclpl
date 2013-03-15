206a04d1a68d8c15e11a3c7342cb0a69123de2a4
Mike D. Lowis <mike@mdlowis.com>
2013-03-15 13:02:17 -0400

    created parser main.c skeleton

--- source/parser/main.c
+++ source/parser/main.c
@@ -1,16 +1,37 @@
-#include "gc.h"
+#include <stdio.h>
+
+int parse_files(int num_files, char** fnames);
+int parse_input(char* outfile);
 
 int main(int argc, char** argv)
 {
-    /* init the collector */
-    int foo;
-    gc_set_stack_base(&foo);
-
-    /* main program */
-
-
-    /* shutdown the collector */
-    gc_shutdown();
-    return 0;
+    int ret;
+    if (argc > 1)
+    {
+        ret = parse_files(argc,argv);
+    }
+    else
+    {
+        ret = parse_input(NULL);
+    }
+    return ret;
 }
+
+int parse_files(int num_files, char** fnames)
+{
+    int ret = 0;
+    int i;
+    for (i = 1; i < num_files; i++)
+    {
+        parse_input(fnames[i]);
+    }
+}
+
+int parse_input(char* outfile)
+{
+    //tok_open(outfile);
+
+    //tok_close();
+}
+
 
